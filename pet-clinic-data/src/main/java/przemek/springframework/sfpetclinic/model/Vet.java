b01e4dace83e1e9dcd4c65d143a5c8f5830f9040
Przemyslaw Cieslik <cprzemek91@hotmail.com>
2019-06-23 21:03:14 +0100

    Updated Vet with Speciality relationship. Closes #18

--- pet-clinic-data/src/main/java/przemek/springframework/sfpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/przemek/springframework/sfpetclinic/model/Vet.java
@@ -1,8 +1,12 @@
 package przemek.springframework.sfpetclinic.model;
 
-import lombok.*;
+import lombok.Data;
+
+import java.util.Set;
 
 
 @Data
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
 }
