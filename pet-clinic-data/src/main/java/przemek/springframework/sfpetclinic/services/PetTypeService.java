cfabcc02f42a59884221d53136db20b49933e147
Przemyslaw Cieslik <cprzemek91@hotmail.com>
2019-06-24 19:11:05 +0100

    Created Pet Type Map Service. Closes #19

---
+++ pet-clinic-data/src/main/java/przemek/springframework/sfpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package przemek.springframework.sfpetclinic.services;
+
+import przemek.springframework.sfpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/przemek/springframework/sfpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package przemek.springframework.sfpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import przemek.springframework.sfpetclinic.model.PetType;
+import przemek.springframework.sfpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findByAll() {
+        return super.findByAll();
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteByid(id);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
