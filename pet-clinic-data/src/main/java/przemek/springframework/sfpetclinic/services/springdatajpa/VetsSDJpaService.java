5048dca85c8a4b5b41dbe2321faa43eb7fd94fd1
Przemyslaw Cieslik <cprzemek91@hotmail.com>
2019-07-04 22:00:13 +0100

    Created Spring Data JPA Service for Vets. Closes #27

---
+++ pet-clinic-data/src/main/java/przemek/springframework/sfpetclinic/services/springdatajpa/VetsSDJpaService.java
@@ -0,0 +1,49 @@
+package przemek.springframework.sfpetclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import przemek.springframework.sfpetclinic.model.Vet;
+import przemek.springframework.sfpetclinic.repositories.VetRepository;
+import przemek.springframework.sfpetclinic.services.VetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetsSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetsSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+
+    @Override
+    public Set<Vet> findByAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
